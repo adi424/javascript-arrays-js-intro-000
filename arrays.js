df41b567590f1ab19ee3a7aa770ac81fb8d9cdb5
adi dabush <adi.dabush@wework.com>
2019-08-22 23:18:23 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,2 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
-function addElementToBeginningOfArray((array, element){
-addElementToBeginningOfArray.unshift(element);
-}
