19df9b522e5137db3443f5a5e76eac3320a1d966
adi dabush <adi.dabush@wework.com>
2019-08-23 00:38:32 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -35,3 +35,7 @@ function destructivelyRemoveElementFromEndOfArray(array, element){
   array.pop(-1);
   return array;
 }
+
+funtion removeElementFromEndOfArray(array, element){
+  return array.slice(-1);
+}
