3c190a794915c2b9f4aa656b665d42966abaf517
adi dabush <adi.dabush@wework.com>
2019-08-23 00:10:41 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -28,5 +28,5 @@ function destructivelyRemoveElementFromBeginningOfArray(array, element){
 }
 
 function removeElementFromBeginningOfArray(array, element){
-  return array.slice(-1);
+  return array.slice(1);
 }
